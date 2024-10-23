f8fd1908ac1ab0722a83a8903fba5e27855475ba
Rajesh kumar <rajesh.kumar@braneenterprises.com>
2024-10-23 17:21:09 +0530

    gitignore

--- src/main/java/businesshousegame/Dice.java
+++ src/main/java/businesshousegame/Dice.java
@@ -5,19 +5,19 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Dice {
-	
-	private Queue<Integer> diceoutputs=new LinkedList<>();
 
-	
-	public Dice(String diceoutputStr) {
-		String[] diceOutputs=diceoutputStr.split(",");
-		Arrays.stream(diceOutputs).forEach(diceoutput ->{
-			Integer diceOutputIntValue=Integer.parseInt(diceoutput);
-			diceoutputs.add(diceOutputIntValue);
-		});		
-	}
-	
-	public Integer giveDiceOutput() {
-		return diceoutputs.poll();
-	}
+    private Queue<Integer> diceoutputs = new LinkedList<>();
+
+
+    public Dice(String diceoutputStr) {
+        String[] diceOutputs = diceoutputStr.split(",");
+        Arrays.stream(diceOutputs).forEach(diceoutput -> {
+            Integer diceOutputIntValue = Integer.parseInt(diceoutput);
+            diceoutputs.add(diceOutputIntValue);
+        });
+    }
+
+    public Integer giveDiceOutput() {
+        return diceoutputs.poll();
+    }
 }
