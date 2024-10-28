6fb98ff728ab03d374381bf3dac0a8f02c5c8a27
Rajesh kumar <rajesh.kumar@braneenterprises.com>
2024-10-28 20:21:36 +0530

    100% coverage

--- src/main/java/businesshousegame/Board.java
+++ src/main/java/businesshousegame/Board.java
@@ -1,31 +1,31 @@
 package businesshousegame;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import businesshousegame.board.cells.Cell;
 import businesshousegame.board.cells.Hotel;
 import businesshousegame.board.cells.Jail;
 import businesshousegame.board.cells.Treasure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
-	private final List<Cell> grid = new ArrayList<>();
+    private final List<Cell> grid = new ArrayList<>();
 
-	public Board(String totalCellCordsAsStr) {
-		String[] cellCords = totalCellCordsAsStr.split(",");
-		for (String cell : cellCords) {
-			switch (cell) {
-			case "E" -> grid.add(null);
-			case "J" -> grid.add(new Jail());
-			case "T" -> grid.add(new Treasure());
-			case "H" -> grid.add(new Hotel());
-			}
-		}
-	}
+    public Board(String totalCellCordsAsStr) {
+        String[] cellCords = totalCellCordsAsStr.split(",");
+        for (String cell : cellCords) {
+            switch (cell) {
+                case "J" -> grid.add(new Jail());
+                case "T" -> grid.add(new Treasure());
+                case "H" -> grid.add(new Hotel());
+                default -> grid.add(null);
+            }
+        }
+    }
 
-	public List<Cell> getGrid() {
-		return grid;
-	}
+    public List<Cell> getGrid() {
+        return grid;
+    }
 
 }
