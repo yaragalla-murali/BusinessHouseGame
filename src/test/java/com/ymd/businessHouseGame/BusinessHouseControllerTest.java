39605309b9d13de6e17d1e44a2db066289340db7
Muralidhar <ymd.muralidhar@gmail.com>
2024-11-22 18:52:11 +0530

    done some changes in test cases

--- src/test/java/com/ymd/businessHouseGame/BusinessHouseControllerTest.java
+++ src/test/java/com/ymd/businessHouseGame/BusinessHouseControllerTest.java
@@ -5,17 +5,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.nio.charset.Charset;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ymd.businesshousegame.controller.BusinessHouseGameController;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.ymd.businesshousegame.entity.Board;
 import com.ymd.businesshousegame.entity.Dice;
 import com.ymd.businesshousegame.entity.Game;
@@ -24,9 +26,9 @@ import com.ymd.businesshousegame.service.BoardService;
 import com.ymd.businesshousegame.service.DiceService;
 import com.ymd.businesshousegame.service.GameService;
 
-@SpringBootTest
+@SpringBootTest 
 @AutoConfigureMockMvc
-@WebMvcTest(BusinessHouseGameController.class)
+//@WebMvcTest(BusinessHouseGameController.class)
 public class BusinessHouseControllerTest {
 
 	@Autowired
@@ -44,6 +46,8 @@ public class BusinessHouseControllerTest {
 	@Autowired
     private ObjectMapper objectMapper;
 	
+	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	
 	
 	@Test
 	void testCreateGame()throws Exception {
@@ -56,10 +60,16 @@ public class BusinessHouseControllerTest {
 		Game game = new Game();
 
 		given(gameService.createGame(board, dice, player)).willReturn(game);
+		
+		ObjectMapper mapper = new ObjectMapper();
+	    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+	    ObjectWriter ow = mapper.writer();//.withDefaultPrettyPrinter();
+	    String requestJson=ow.writeValueAsString(player );
 
 		mockMvc
 	      .perform(post("/games")
-	      .contentType(MediaType.APPLICATION_JSON)
+	      .contentType(APPLICATION_JSON_UTF8)
+	      .content(requestJson)
 	      .accept(MediaType.APPLICATION_JSON))
 	      .andExpect(status().isOk())
 	      .andExpect(content().json(objectMapper.writeValueAsString(game)));
