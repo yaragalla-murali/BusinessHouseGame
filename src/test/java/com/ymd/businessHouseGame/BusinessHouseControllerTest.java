f927a761ff77a44c7e3f7b3d84279fc08157e2c8
Rajesh kumar <rajesh.kumar@braneenterprises.com>
2024-11-22 20:00:48 +0530

    Murali the hardworkers test is passing now

--- src/test/java/com/ymd/businessHouseGame/BusinessHouseControllerTest.java
+++ src/test/java/com/ymd/businessHouseGame/BusinessHouseControllerTest.java
@@ -1,74 +1,90 @@
 package com.ymd.businessHouseGame;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.ymd.businesshousegame.controller.BusinessHouseGameController;
+import com.ymd.businesshousegame.entity.Board;
+import com.ymd.businesshousegame.entity.Dice;
+import com.ymd.businesshousegame.entity.Game;
+import com.ymd.businesshousegame.entity.Player;
+import com.ymd.businesshousegame.model.GameStatus;
+import com.ymd.businesshousegame.service.BoardService;
+import com.ymd.businesshousegame.service.DiceService;
+import com.ymd.businesshousegame.service.GameService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.nio.charset.Charset;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.ymd.businesshousegame.entity.Board;
-import com.ymd.businesshousegame.entity.Dice;
-import com.ymd.businesshousegame.entity.Game;
-import com.ymd.businesshousegame.entity.Player;
-import com.ymd.businesshousegame.service.BoardService;
-import com.ymd.businesshousegame.service.DiceService;
-import com.ymd.businesshousegame.service.GameService;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-//@WebMvcTest(BusinessHouseGameController.class)
+@WebMvcTest(BusinessHouseGameController.class)
 public class BusinessHouseControllerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@MockBean
-	private GameService gameService;
+    @MockBean
+    private GameService gameService;
 
-	@MockBean
-	private BoardService boardService;
+    @MockBean
+    private BoardService boardService;
 
-	@MockBean
-	private DiceService diceService;
+    @MockBean
+    private DiceService diceService;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Test
-	void testCreateGame() throws Exception {
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
-		Board board = boardService.setupBoard(
-				"E,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E," + "H,J,T,H,J,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E");
-		Dice dice = diceService.setup("4,4,4,6,7,8,5,11,10,12,2,3,5,6,7,8,5,11,10,12,2," + "3,5,6,7,8,5,11,10,12");
-		Player player = new Player();
-		player.setName("lakshman");
-		Game game = new Game();
 
-		given(gameService.createGame(board, dice, player)).willReturn(game);
+    @Test
+    void testCreateGame() throws Exception {
 
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-		String requestJson = ow.writeValueAsString(player);
+        Board board = boardService.setupBoard(
+                "E,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E," + "H,J,T,H,J,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E");
+        Dice dice = diceService.setup("4,4,4,6,7,8,5,11,10,12,2,3,5,6,7,8,5,11,10,12,2," + "3,5,6,7,8,5,11,10,12");
+        Player player = new Player();
+        player.setName("lakshman");
+        Game game = new Game();
+        game.setBoard(board);
+        game.setDice(dice);
+        List<Player> players = Arrays.asList(player);
+        game.setPlayers(players);
+        game.setNextPlayer(player);
+        game.setStatus(GameStatus.CREATED);
 
-		mockMvc.perform(post("/games")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(requestJson)
-			    .accept(MediaType.APPLICATION_JSON))
-		        .andExpect(status().isOk())
-				.andExpect(content().json(objectMapper.writeValueAsString(game)));
-	}
+        given(gameService.createGame(any(), any(), any())).willReturn(game);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer();//.withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(player);
+
+        ResultActions perform = mockMvc
+                .perform(post("/games")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(requestJson)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        System.out.println("perform.toString() = " + perform.andReturn().getResponse().getContentAsString());
+
+        perform.andExpect(status().isOk());
+
+        perform.andExpect(content().json(objectMapper.writeValueAsString(game)));
+    }
 
 }
