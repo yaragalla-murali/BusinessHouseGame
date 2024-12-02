73b91defa2e71149d881815e1e45e052b0419059
Muralidhar <ymd.muralidhar@gmail.com>
2024-12-02 13:58:31 +0530

    completed remaining unit tests

--- src/main/java/com/ymd/businesshousegame/service/DiceService.java
+++ src/main/java/com/ymd/businesshousegame/service/DiceService.java
@@ -1,38 +1,36 @@
 package com.ymd.businesshousegame.service;
 
-import com.ymd.businesshousegame.entity.Dice;
-import com.ymd.businesshousegame.repository.DiceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ymd.businesshousegame.entity.Dice;
+import com.ymd.businesshousegame.repository.DiceRepository;
+
 @Service
 public class DiceService {
 
-    @Autowired
-    private DiceRepository diceRepo;
+	@Autowired
+	private DiceRepository diceRepo;
 
-    public Dice setup(String diceOutputsStr) {
-        return diceRepo.save(new Dice(getDiceOutputs(diceOutputsStr)));
-    }
+	public Dice setup(String diceOutputsStr) {
+		return diceRepo.save(new Dice(getDiceOutputs(diceOutputsStr)));
+	}
 
-    private static List<Integer> getDiceOutputs(String diceOutputsStr) {
-        return Arrays.stream(diceOutputsStr.split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
+	private static List<Integer> getDiceOutputs(String diceOutputsStr) {
+		return Arrays.stream(diceOutputsStr.split(",")).map(String::trim).map(Integer::parseInt)
+				.collect(Collectors.toList());
+	}
 
-    public int giveDice(Dice dice) {
-        int currentOutputIndex = dice.getLastUsedOutputIndex() + 1;
-        int output = dice.getOutputs().get(currentOutputIndex);
-        dice.setLastUsedOutputIndex(currentOutputIndex);
-        diceRepo.save(dice);
-        return output;
-    }
+	public int giveDice(Dice dice) {
+		int currentOutputIndex = dice.getLastUsedOutputIndex() + 1;
+		int output = dice.getOutputs().get(currentOutputIndex);
+		dice.setLastUsedOutputIndex(currentOutputIndex);
+		diceRepo.save(dice);
+		return output;
+	}
 
 }
--- src/test/java/com/ymd/businesshousegame/service/CellServiceTest.java
+++ src/test/java/com/ymd/businesshousegame/service/CellServiceTest.java
@@ -1,56 +1,176 @@
 package com.ymd.businesshousegame.service;
 
-import com.ymd.businesshousegame.entity.Board;
-import com.ymd.businesshousegame.entity.Cell;
-import com.ymd.businesshousegame.entity.CellType;
-import com.ymd.businesshousegame.entity.Player;
-import com.ymd.businesshousegame.repository.CellRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import com.ymd.businesshousegame.entity.Board;
+import com.ymd.businesshousegame.entity.Cell;
+import com.ymd.businesshousegame.entity.CellType;
+import com.ymd.businesshousegame.entity.Player;
+import com.ymd.businesshousegame.repository.CellRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class CellServiceTest {
 
-    @Mock
-    private CellRepository cellDao;
+	@Mock
+	private CellRepository cellDao;
 
-    @InjectMocks
-    private CellService cellService;
+	@InjectMocks
+	private CellService cellService;
 
-    @Test
-    void testHandleCellLanding() {
+	@Test
+	void testHandleCellLandingTreasureCell() {
 
-        String boardCords = "T,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E";
-        String[] boardCordsAsArray = boardCords.split(",");
-        List<Cell> boardCells = new ArrayList<>();
-        int counter = 0;
-        for (String cord : boardCordsAsArray) {
-            switch (cord) {
-                case "J" -> boardCells.add(new Cell(CellType.JAIL, counter));
-                case "T" -> boardCells.add(new Cell(CellType.TREASURE, counter));
-                case "H" -> boardCells.add(new Cell(CellType.HOTEL, counter));
-                case "E" -> boardCells.add(new Cell(CellType.EMPTY, counter));
-            }
-            counter = counter + 1;
-        }
+		String boardCords = "T,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E";
+		String[] boardCordsAsArray = boardCords.split(",");
+		List<Cell> boardCells = new ArrayList<>();
+		int counter = 0;
+		for (String cord : boardCordsAsArray) {
+			switch (cord) {
+			case "J" -> boardCells.add(new Cell(CellType.JAIL, counter));
+			case "T" -> boardCells.add(new Cell(CellType.TREASURE, counter));
+			case "H" -> boardCells.add(new Cell(CellType.HOTEL, counter));
+			case "E" -> boardCells.add(new Cell(CellType.EMPTY, counter));
+			}
+			counter = counter + 1;
+		}
 
-        Board board = new Board(1, boardCells);
+		Board board = new Board(1, boardCells);
 
-        Player player = new Player(1, "Test Player");
+		Player player = new Player(1, "Test Player");
 
-        Player resultPlayer = cellService.handleCellLanding(board, player);
+		Player resultPlayer = cellService.handleCellLanding(board, player);
 
-        assertNotNull(resultPlayer);
-        assertEquals(1200, resultPlayer.getTotalBalance());
+		assertNotNull(resultPlayer);
+		assertEquals(1200, resultPlayer.getTotalBalance());
 
-    }
+	}
+
+	@Test
+	void testHandleCellLandingHotelCell() {
+
+		String boardCords = "H,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E";
+		String[] boardCordsAsArray = boardCords.split(",");
+		List<Cell> boardCells = new ArrayList<>();
+		int counter = 0;
+		for (String cord : boardCordsAsArray) {
+			switch (cord) {
+			case "J" -> boardCells.add(new Cell(CellType.JAIL, counter));
+			case "T" -> boardCells.add(new Cell(CellType.TREASURE, counter));
+			case "H" -> boardCells.add(new Cell(CellType.HOTEL, counter));
+			case "E" -> boardCells.add(new Cell(CellType.EMPTY, counter));
+			}
+			counter = counter + 1;
+		}
+
+		Board board = new Board(1, boardCells);
+
+		Player player = new Player(1, "Test Player");
+
+		Player resultPlayer = cellService.handleCellLanding(board, player);
+
+		assertNotNull(resultPlayer);
+		assertEquals(800, resultPlayer.getTotalBalance());
+		assertNotNull(board.getBoardCells().get(0).getHotelOwner());
+
+	}
+
+	@Test
+	void testHandleCellLandingPreOwnedHotelCell() {
+
+		String boardCords = "H,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E";
+		String[] boardCordsAsArray = boardCords.split(",");
+		Player player = new Player(1, "Test Player");
+
+		List<Cell> boardCells = new ArrayList<>();
+		int counter = 0;
+		for (String cord : boardCordsAsArray) {
+			switch (cord) {
+			case "J" -> boardCells.add(new Cell(CellType.JAIL, counter));
+			case "T" -> boardCells.add(new Cell(CellType.TREASURE, counter));
+			case "H" -> {
+				Cell hotelCell = new Cell(CellType.HOTEL, counter);
+				hotelCell.setHotelOwner(player);
+				boardCells.add(hotelCell);
+			}
+
+			case "E" -> boardCells.add(new Cell(CellType.EMPTY, counter));
+			}
+			counter = counter + 1;
+		}
+
+		Board board = new Board(1, boardCells);
+
+		Player resultPlayer = cellService.handleCellLanding(board, player);
+
+		assertNotNull(resultPlayer);
+		assertEquals(950, resultPlayer.getTotalBalance());
+		assertNotNull(board.getBoardCells().get(0).getHotelOwner());
+
+	}
+
+	@Test
+	void testHandleCellLandingJailCell() {
+
+		String boardCords = "J,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E";
+		String[] boardCordsAsArray = boardCords.split(",");
+		List<Cell> boardCells = new ArrayList<>();
+		int counter = 0;
+		for (String cord : boardCordsAsArray) {
+			switch (cord) {
+			case "J" -> boardCells.add(new Cell(CellType.JAIL, counter));
+			case "T" -> boardCells.add(new Cell(CellType.TREASURE, counter));
+			case "H" -> boardCells.add(new Cell(CellType.HOTEL, counter));
+			case "E" -> boardCells.add(new Cell(CellType.EMPTY, counter));
+			}
+			counter = counter + 1;
+		}
+
+		Board board = new Board(1, boardCells);
+
+		Player player = new Player(1, "Test Player");
+
+		Player resultPlayer = cellService.handleCellLanding(board, player);
+
+		assertNotNull(resultPlayer);
+		assertEquals(850, resultPlayer.getTotalBalance());
+
+	}
+
+	@Test
+	void testHandleCellLandingEmptyCell() {
+
+		String boardCords = "E,E,J,H,E,T,J,T,E,E,H,J,T,H,E,E,J,H,E,T,J,T,E,E,H,J,T,E,H,E";
+		String[] boardCordsAsArray = boardCords.split(",");
+		List<Cell> boardCells = new ArrayList<>();
+		int counter = 0;
+		for (String cord : boardCordsAsArray) {
+			switch (cord) {
+			case "J" -> boardCells.add(new Cell(CellType.JAIL, counter));
+			case "T" -> boardCells.add(new Cell(CellType.TREASURE, counter));
+			case "H" -> boardCells.add(new Cell(CellType.HOTEL, counter));
+			case "E" -> boardCells.add(new Cell(CellType.EMPTY, counter));
+			}
+			counter = counter + 1;
+		}
+
+		Board board = new Board(1, boardCells);
+
+		Player player = new Player(1, "Test Player");
+
+		Player resultPlayer = cellService.handleCellLanding(board, player);
+
+		assertNotNull(resultPlayer);
+		assertEquals(1000, resultPlayer.getTotalBalance());
+
+	}
 }
